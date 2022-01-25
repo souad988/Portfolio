9b5370ca0f160befe74fa04be5203ae558706e9d
Mnemba Chambuya <karimnembac@gmail.com>
2022-01-25 13:52:05 +0300

    style the mobile nav menu list

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,15 @@ const navLinks = document.querySelector('.nav-links');
 const closeIcon = document.querySelector('.close_icon');
 const welcome = document.querySelector('.welcome');
 const container = document.querySelector('.menu_list_container');
+const mobileNavLinks = document.querySelectorAll('.nav-link');
+
+function closeMobileNav() {
+  menuIcon.classList.remove('hide');
+  navLinks.classList.remove('show-list');
+  closeIcon.classList.remove('show-icon');
+  welcome.classList.remove('hide');
+  container.classList.remove('show-menu_list_container');
+}
 
 menuIcon.addEventListener('click', () => {
   menuIcon.classList.add('hide');
@@ -12,10 +21,9 @@ menuIcon.addEventListener('click', () => {
   container.classList.add('show-menu_list_container');
 });
 
-closeIcon.addEventListener('click', () => {
-  menuIcon.classList.remove('hide');
-  navLinks.classList.remove('show-list');
-  closeIcon.classList.remove('show-icon');
-  welcome.classList.remove('hide');
-  container.classList.remove('show-menu_list_container');
-});
+
+
+closeIcon.addEventListener('click',closeMobileNav);
+
+navLinks.addEventListener('click', closeMobileNav);
+
