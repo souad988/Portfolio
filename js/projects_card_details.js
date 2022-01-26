fd1c63e8d40651253a227c2d3773e92fb8dcbebb
souad988 <59707859+souad988@users.noreply.github.com>
2022-01-26 14:39:57 +0100

    Fix js linters

--- js/projects_card_details.js
+++ js/projects_card_details.js
@@ -1,20 +1,20 @@
 /* eslint-disable space-before-blocks */
 const cardDetailsContainer = document.querySelector('#work_project_details');
 const array1 = ['youtube', 'portfolio', 'ecom', 'youtube1', 'portfolio1', 'ecom1'];
-const workCloseIcon= document.querySelector('.work_detail_close_icon');
+const workCloseIcon = document.querySelector('.work_detail_close_icon');
 /* project card detail elements */
 const projectImg = document.querySelector('.work_project_details_img');
 const projectTitle = document.querySelector('.work_project_details_title');
 const projectDescription = document.querySelector('.work_project_details_description');
 const projectTechnologies = document.querySelector('.work_project_details_technologies');
-const projectLinkToSource_mobile = document.querySelector('.btns_bottom_live');
-const projectLinkToLive_mobile = document.querySelector('.btns_bottom_github');
-const projectLinkToSource_desktop = document.querySelector('.btns_title_live');
-const projectLinkToLive_desktop = document.querySelector('.btns_title_github');
+const projectLinkToSourcemobile = document.querySelector('.btns_bottom_live');
+const projectLinkToLivemobile = document.querySelector('.btns_bottom_github');
+const projectLinkToSourcedesktop = document.querySelector('.btns_title_live');
+const projectLinkToLivedesktop = document.querySelector('.btns_title_github');
 
 function createListItems(technologies){
-  let teckList = [];  
-  technologies.forEach((item)=>{
+  const teckList = [];
+  technologies.forEach((item) => {
     const para = document.createElement('p');
     const node = document.createTextNode(item);
     para.appendChild(node);
@@ -26,29 +26,28 @@ function createListItems(technologies){
   return teckList;
 }
 
-function fillCardDetail(projectDetail,img,title,description,technologies,live_mobile,github_mobile,live_desktop,github_desktop){
-  img.src = projectDetail['image'];
-  title.innerHTML = projectDetail['name'];
-  description.innerHTML = projectDetail['description'];
-  live_mobile.onclick = 'window.location.href = '+projectDetail['linkToLiveVersion'];
-  live_desktop.onclick = function (){ location.href = projectDetail['linkToLiveVersion']};
-  github_mobile.onclick = function (){ location.href = projectDetail['LinkToSource']};
-  github_desktop.onclick = function (){ location.href = projectDetail['LinkToSource']};
+function fillCardDetail(projectDetail, img, title, description, technologies, livemobile,
+  githubmobile, livedesktop, githubdesktop){
+  img.src = projectDetail.image;
+  title.innerHTML = projectDetail.name;
+  description.innerHTML = projectDetail.description;
+
+  livemobile.onclick = `"window.location.href = '${projectDetail.linkToLiveVersion}';"`;
+  livedesktop.onclick = () => { window.location.href = projectDetail.linkToLiveVersion; };
+  githubmobile.onclick = () => { window.location.href = projectDetail.LinkToSource; };
+  githubdesktop.onclick = () => { window.location.href = projectDetail.LinkToSource; };
+
   let teckList1 = [];
-  console.log(teckList1);
-  teckList1 = createListItems(projectDetail['technologies']);
-  console.log(teckList1);
-  teckList1.forEach(elmnt=>{technologies.appendChild(elmnt);
-  console.log(technologies);
-});
+  teckList1 = createListItems(projectDetail.technologies);
+  teckList1.forEach((elmnt) => { technologies.appendChild(elmnt); });
 }
 
 function createCardDetail(projectName) {
-  console.log(projectName);
   const projectDetail = projectsDetail.filter((item) => item.name === projectName)[0];
-  console.log(projectDetail);
   cardDetailsContainer.classList.add('show_work_project_details');
-  fillCardDetail(projectDetail,projectImg,projectTitle,projectDescription,projectTechnologies,projectLinkToLive_mobile,projectLinkToSource_mobile,projectLinkToLive_desktop,projectLinkToSource_desktop);
+  fillCardDetail(projectDetail, projectImg, projectTitle, projectDescription,
+    projectTechnologies, projectLinkToLivemobile, projectLinkToSourcemobile,
+    projectLinkToLivedesktop, projectLinkToSourcedesktop);
 }
 
 /* add event listner to each button */
@@ -59,6 +58,6 @@ array1.forEach((btn) => {
 });
 
 workCloseIcon.addEventListener('click', () => {
-    cardDetailsContainer.classList.remove('show_work_project_details');
-    projectTechnologies.innerHTML = '';
+  cardDetailsContainer.classList.remove('show_work_project_details');
+  projectTechnologies.innerHTML = '';
 });
