b12aa931ce5367dd29a0ca736e822471b44df960
souad988 <59707859+souad988@users.noreply.github.com>
2022-01-26 13:57:41 +0100

    Create variables for buttons

--- js/projects_card_details.js
+++ js/projects_card_details.js
@@ -7,8 +7,10 @@ const projectImg = document.querySelector('.work_project_details_img');
 const projectTitle = document.querySelector('.work_project_details_title');
 const projectDescription = document.querySelector('.work_project_details_description');
 const projectTechnologies = document.querySelector('.work_project_details_technologies');
-const projectLinkToSource = document.querySelector('.work_project_details_img');
-const projectLinkToLive = document.querySelector('.work_project_details_img');
+const projectLinkToSource_mobile = document.querySelector('.btns_bottom_live');
+const projectLinkToLive_mobile = document.querySelector('.btns_bottom_github');
+const projectLinkToSource_desktop = document.querySelector('.btns_title_live');
+const projectLinkToLive_desktop = document.querySelector('.btns_title_github');
 
 function createListItems(technologies){
   let teckList = [];  
@@ -24,10 +26,14 @@ function createListItems(technologies){
   return teckList;
 }
 
-function fillCardDetail(projectDetail,img,title,description,technologies){
+function fillCardDetail(projectDetail,img,title,description,technologies,live_mobile,github_mobile,live_desktop,github_desktop){
   img.src = projectDetail['image'];
   title.innerHTML = projectDetail['name'];
   description.innerHTML = projectDetail['description'];
+  live_mobile.onclick = 'window.location.href = '+projectDetail['linkToLiveVersion'];
+  live_desktop.onclick = function (){ location.href = projectDetail['linkToLiveVersion']};
+  github_mobile.onclick = function (){ location.href = projectDetail['LinkToSource']};
+  github_desktop.onclick = function (){ location.href = projectDetail['LinkToSource']};
   let teckList1 = [];
   console.log(teckList1);
   teckList1 = createListItems(projectDetail['technologies']);
@@ -42,7 +48,7 @@ function createCardDetail(projectName) {
   const projectDetail = projectsDetail.filter((item) => item.name === projectName)[0];
   console.log(projectDetail);
   cardDetailsContainer.classList.add('show_work_project_details');
-  fillCardDetail(projectDetail,projectImg,projectTitle,projectDescription,projectTechnologies);
+  fillCardDetail(projectDetail,projectImg,projectTitle,projectDescription,projectTechnologies,projectLinkToLive_mobile,projectLinkToSource_mobile,projectLinkToLive_desktop,projectLinkToSource_desktop);
 }
 
 /* add event listner to each button */
