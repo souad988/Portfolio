86475792749e8e5877343472dec8621acfb850b9
souad988 <59707859+souad988@users.noreply.github.com>
2022-01-27 07:24:01 +0100

    Fix the project detail desktop styling

--- js/projects_card_details.js
+++ js/projects_card_details.js
@@ -1,7 +1,7 @@
 const projectsDetail = [{
   LinkToSource: 'https://github.com/souad988/youtube',
   linkToLiveVersion: 'https://souad988.github.io/youtube/',
-  image: 'media/images/projects/youtube.png',
+  image: '../media/images/projects/youtube.png',
   description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
   name: 'youtube',
   technologies: ['Html', 'css'],
@@ -9,7 +9,7 @@ const projectsDetail = [{
 {
   LinkToSource: 'https://github.com/souad988/Portfolio',
   linkToLiveVersion: 'https://souad988.github.io/Portfolio/',
-  image: 'media/images/projects/portfolio.PNG',
+  image: '../media/images/projects/portfolio.PNG',
   description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
   name: 'portfolio',
   technologies: ['Html', 'css', 'javascript'],
